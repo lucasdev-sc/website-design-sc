e2e7270053b9c54895d6fba8f4fe8a2ffed1d051
Lucas Cauã <byte4656@gmail.com>
2023-06-27 21:47:50 -0300

    commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path={"/" || "/website-design-sc"} element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/products" element={<Products />} />
           <Route path="/sign-up" element={<SignUp />} />
